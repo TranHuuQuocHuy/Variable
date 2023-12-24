e78bb689fca7f979397d2b111d71e4ae44ac90e0
quochuy232005 <144077996+TranHuuQuocHuy@users.noreply.github.com>
2023-12-24 15:48:43 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,28 @@
+package basic.dev;
+
+/**
+ * 
+ * @author TranThanhBinh
+ *
+ */
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/**
+		 * Khai bao bien
+		 */
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true;
+
+		System.out.println("Ho va ten: ");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Mark Math: " + markMath);
+		System.out.println("Passed: " + passed);
+		final float PI = 3.14f;
+	}
+}
